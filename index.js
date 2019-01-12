7969fd56b5c84e34f56404b90b3f0ed042628352
Robert Rees <shudderfix@gmail.com>
2019-01-12 23:06:07 +0000

    Chooses a unique relationship each time

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const random = require('rrees-js-random');
 
 function generateMap(actors, relationships) {
     const relationshipMap = [];
+    const shuffledRelationships = random.shuffle(relationships);
     for(relationship in relationships) {
         relationshipMap.push({
             parties: [actors[0], actors[1]],
-            nature: relationships[0],
+            nature: shuffledRelationships.pop(),
             mutual: Math.random() > 0.2
         });
     }
@@ -15,3 +17,6 @@ function generateMap(actors, relationships) {
 module.exports = {
     generateMap
 };
+
+const generatedMap = generateMap(['Man', 'Woman'], ['Love', 'Hate', 'Former friends']);
+console.log(generatedMap);
